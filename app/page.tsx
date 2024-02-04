89303354272450233785402444fbf476f218b7f2
Shadin Muhammed <108604275+shadinmhd@users.noreply.github.com>
2024-02-04 12:13:30 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+import Navbar from "@/components/Navbar"
+import Subscribe from "@/components/home/Subscribe"
+
+const Home = () => {
+
+	return (
+		<div className="flex bg-black h-screen w-screen flex-col items-center justify-center">
+			<Navbar />
+			<div className="flex flex-col text-white font-extrabold gap-5 items-center justify-center h-full w-full">
+				<div className="flex flex-col items-center justify-center gap-4">
+					<p className="text-8xl">
+						Blog
+					</p>
+					<p className="text-xl opacity-80">
+						Welcome to my personal blog, here i share my experience and perspective on all thing tech
+					</p>
+				</div>
+				<Subscribe />
+			</div>
+		</div>
+	)
+}
+
+export default Home
