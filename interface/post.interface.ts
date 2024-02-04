a4428f367f7f0dab123936cdae46d671a14db46b
Shadin Muhammed <108604275+shadinmhd@users.noreply.github.com>
2024-02-04 12:15:34 +0530

    new: post model and interface

---
+++ interface/post.interface.ts
@@ -0,0 +1,9 @@
+interface PostInterface {
+	_id?: string,
+	title?: string,
+	description?: string,
+	createdAt?: Date,
+	updatedAt?: Date
+}
+
+export default PostInterface
---
+++ model/post.model.ts
@@ -0,0 +1,17 @@
+import PostInterface from "@/interface/post.interface";
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema<PostInterface>({
+	title: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	description: {
+		type: String,
+		required: true
+	}
+}, { timestamps: true })
+
+const PostModel = mongoose.models.Post as mongoose.Model<PostInterface> || mongoose.model("Post", postSchema)
+export default PostModel
