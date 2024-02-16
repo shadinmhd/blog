878346c46ff9831e293e6389b1537d190417096e
Shadin Muhammed <108604275+shadinmhd@users.noreply.github.com>
2024-02-16 18:48:07 +0530

    new: axios error handler for frontend

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,17 @@
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
+import { toast } from "sonner";
 
 const api = axios.create({})
+
+export const handleAxiosError = (error: any) => {
+	if (isAxiosError(error))
+		if (error.response?.data.message)
+			toast.error(error.response.data.message)
+		else
+			toast.error(error.message)
+	else
+		toast.error("something went wrong")
+	console.log(error)
+}
 
 export default api
