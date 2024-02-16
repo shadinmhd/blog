31c9a2f7a2ad63f10bb15dc372ea962ebb212f2f
Shadin Muhammed <108604275+shadinmhd@users.noreply.github.com>
2024-02-16 18:50:02 +0530

    new: style change

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,23 @@ import Subscribe from "@/components/home/Subscribe"
 const Home = () => {
 
 	return (
-		<div className="flex bg-black h-screen w-screen flex-col items-center justify-center">
-			<Navbar />
-			<div className="flex flex-col text-white font-extrabold gap-5 items-center justify-center h-full w-full">
-				<div className="flex flex-col items-center justify-center gap-4">
-					<p className="text-8xl">
-						Blog
-					</p>
-					<p className="text-xl opacity-80">
-						Welcome to my personal blog, here i share my experience and perspective on all thing tech
-					</p>
+		<div className="flex flex-col overflow-x-hidden bg-black">
+			<div className="flex bg-black h-screen w-screen flex-col items-center justify-center">
+				<Navbar />
+				<div className="flex flex-col text-white font-extrabold gap-5 items-center justify-center h-full w-full">
+					<div className="flex flex-col items-center justify-center gap-4">
+						<p className="text-8xl">
+							Blog
+						</p>
+						<p className="text-xl opacity-80">
+							Welcome to my personal blog, here i share my experience and perspective on all thing tech
+						</p>
+					</div>
+					<Subscribe />
 				</div>
-				<Subscribe />
+			</div>
+			<div className="flex bg-black h-screen w-screen flex-col items-center justify-center">
+
 			</div>
 		</div>
 	)
