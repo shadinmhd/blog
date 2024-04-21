265d71d0b2c45293f3c9ac1f9f6072d3340dc559
shadinmhd <shadinmhd97@gmail.com>
2024-04-21 20:11:16 +0530

    fix: resolved all the build errors from previous commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import Provider from "@/components/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +15,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
 		<html lang="en">
 			<body className={inter.className}>
 				<Toaster richColors={true} />
-				<Provider>
-					{children}
-				</Provider>
+				{children}
 			</body>
 		</html>
 	);
