84607c0a61e05e0621160f82e478f66051a718ff
shadinmhd <shadinmhd97@gmail.com>
2024-04-24 20:09:38 +0530

    fix: removed unneccesery type

--- interface/post.interface.ts
+++ interface/post.interface.ts
@@ -1,11 +1,11 @@
 interface PostInterface {
-	_id?: string,
-	title?: string,
-	description?: string,
-	thumbnail?: string,
-	content?:string,
-	createdAt?: Date,
-	updatedAt?: Date
+	_id: string,
+	title: string,
+	description: string,
+	thumbnail: string,
+	content: string,
+	createdAt: Date,
+	updatedAt: Date
 }
 
 export default PostInterface
