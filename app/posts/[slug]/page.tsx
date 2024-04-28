ab4efa6a514a0377a52dd2bad1e022a175e3e7b5
shadinmhd <shadinmhd97@gmail.com>
2024-04-28 12:43:39 +0530

    update: post view page

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { format, parseISO } from 'date-fns'
 import { allPosts } from 'contentlayer/generated'
 import { notFound } from 'next/navigation'
-import "./local.css"
+import "./style.css"
 
 export const generateStaticParams = async () => allPosts.map((post) => ({ slug: post._raw.flattenedPath }))
 
@@ -16,12 +16,12 @@ const PostLayout = ({ params }: { params: { slug: string } }) => {
 	if (!post) notFound()
 
 	return (
-		<article className="mx-auto max-w-xl py-8">
-			<div className="mb-8 text-center">
+		<article className="mx-auto flex flex-col items-start font-semibold text-lg text-primary max-w-2xl py-8 pt-24">
+			<div className="mb-8 text-start">
+				<h1 className="text-4xl font-bold">{post.title}</h1>
 				<time dateTime={post.date} className="mb-1 text-xs text-gray-600">
 					{format(parseISO(post.date), 'LLLL d, yyyy')}
 				</time>
-				<h1 className="text-3xl font-bold">{post.title}</h1>
 			</div>
 			<div className="[&>*]:mb-3 [&>*:last-child]:mb-0" dangerouslySetInnerHTML={{ __html: post.body.html }} />
 		</article>
