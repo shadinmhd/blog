64e990108b53a0dcfb39db7742df086677d13d53
shadinmhd <shadinmhd97@gmail.com>
2024-05-02 08:27:49 +0530

    update: added my own favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
 	title: "Shadinmhd",
 	description: "",
+	icons: "/photo.png"
 };
 
 export default function RootLayout({
